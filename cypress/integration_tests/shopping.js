95e4ebf4e91d222f17615aeac11cc3575df6a60d
d.stanisavljevic <dragan.stanisavljevic@leaseplan.com>
2023-08-26 21:42:55 +0200

    refactoring

--- cypress/integration_tests/shopping.js
+++ cypress/integration_tests/shopping.js
@@ -11,7 +11,22 @@ describe('Shopping feature', function() {
     let first_product_end_point = '/actions/Catalog.action?viewProduct=&productId=K9-BD-01'
     let second_product_end_point = '/actions/Catalog.action?viewProduct=&productId=RP-SN-01'
     const signInPage = new SignInPage()
-    this.sum = 0.0
+    //this.sum = 0.0
+
+    function test($el, index, $list, productName){
+        cy.log("INDEX = " + index)
+        if(index > 0 && index < $list.length - 1){
+            const product =  $el.find('td:nth-child(3)').text()
+            if(product.includes(productName)){
+                cy.wrap(true).as('isFound')
+                const price = $el.find("td:nth-child(4)").text()
+                cy.get('@sum').then(function(value){
+                    cy.wrap(parseFloat(value) + parseFloat(price)).as('sum')
+                })
+                cy.wrap($el).find("td:nth-child(5)").find('.Button').click()
+            }
+        }     
+    }
 
     beforeEach(function() { 
         cy.fixture("users").then(function(data){
@@ -21,50 +36,22 @@ describe('Shopping feature', function() {
         })  
       })
 
-      it('Check shopping 1', function() {  
-        loginByForm("YY", "UU")
-      })
+      it('Check shopping', function() {              
+        cy.wrap(0.0).as('sum') 
+        cy.wrap(false).as('isFound')
 
-      it('Check shopping', function() { 
-        cy.log("USERNAME = " + this.myusername)   
-             
-        this.sum = 0.0   
-        cy.visit(Cypress.env('url') + first_product_end_point)
-        let isFound = false
-        cy.wrap(isFound).as('found')
+        cy.visit(Cypress.env('url') + first_product_end_point)        
         cy.get('tbody tr', { timeout: 5000 }).each(($el, index, $list) => {
-            cy.log("INDEX = " + index)
-            if(index > 0 && index < $list.length - 1){
-                const product =  $el.find('td:nth-child(3)').text()
-                if(product.includes("Male Adult")){
-                    isFound = true
-                    cy.wrap(true).as('found')
-                    const price = $el.find("td:nth-child(4)").text()
-                    this.sum = this.sum + parseFloat(price)
-                    cy.wrap($el).find("td:nth-child(5)").find('.Button').click()
-                }
-            }          
+                test($el, index, $list, "Male Adult")
           })
 
-        isFound = false
-        cy.get('@found').then(function(value){
-            cy.log("value = " + value)
-        })
+        cy.wrap(false).as('isFound')
 
         cy.visit(Cypress.env('url') + second_product_end_point)
         cy.get('tbody tr').each(($el, index, $list) => { 
-            if(index > 0 && index < $list.length - 1){
-                const product =  $el.find('td:nth-child(3)').text()
-                if(product.includes("Venomless")){
-                    isFound = true
-                    const price = $el.find("td:nth-child(4)").text()
-                    this.sum = parseFloat(this.sum) + parseFloat(price)
-                    cy.wrap($el).find("td:nth-child(5)").find('.Button').click()
-                }
-            }
+           test($el, index, $list, "Venomless")
         })
 
-        let finalPrice
         cy.get('[colspan="7"]').then(function(data){
             expect(this.sum).to.be.equal(parseFloat(data.text().substring(12)))          
         })
